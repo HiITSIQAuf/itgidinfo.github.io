3d61f9c0e0c0331cd16f87904f05ee089f5ec260
HiITSIQAuf <110158108+HiITSIQAuf@users.noreply.github.com>
2022-09-09 16:36:06 +0500

    Hello

---
+++ build/precache-manifest.3d35e3ef26661e7b294b64c533121f50.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f09f515b167ef7778118",
+    "url": "/static/css/main.fe207978.chunk.css"
+  },
+  {
+    "revision": "f09f515b167ef7778118",
+    "url": "/static/js/main.f09f515b.chunk.js"
+  },
+  {
+    "revision": "7a246d37b009df694a37",
+    "url": "/static/js/1.7a246d37.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "833eaaf76cb3601066ac33fcd40e7aa7",
+    "url": "/static/media/demo.833eaaf7.png"
+  },
+  {
+    "revision": "5099a42571038f8cc2550e6d922a9b6c",
+    "url": "/static/media/using.5099a425.png"
+  },
+  {
+    "revision": "3360cb3c51edee12b9dfff30094d4981",
+    "url": "/static/media/of.3360cb3c.png"
+  },
+  {
+    "revision": "e1f6165df16fc6f27139e9a3c35adafd",
+    "url": "/static/media/website.e1f6165d.png"
+  },
+  {
+    "revision": "47bd1eea6b48b2cd1c33516fd7c8f220",
+    "url": "/static/media/or.47bd1eea.png"
+  },
+  {
+    "revision": "b9f00d385a4938f37b20f6c76f24cbf8",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import { Header } from './component/header'
+import { Section } from './component/section/section';
+import './App.css'
+
+ function App() {
+  return (
+    <div className='App'>
+        <Header />
+        <Section />
+    </div>
+  )
+}
+
+export default App;
